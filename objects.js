fcc7e9ab70a910f037fd411e7596aaedf80d863f
Mark Sarcone <mdsarcone@gmail.com>
2017-09-05 00:24:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {}
 function updatePlaylist(playlist,artistName,songTitle) {
   return Object.assign({}, playlist, {artistName: songTitle})
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist['artistName']
+}
