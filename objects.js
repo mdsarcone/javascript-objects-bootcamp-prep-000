c10aaf88ef6465b039ae008d0df646d4aa3e97da
Mark Sarcone <mdsarcone@gmail.com>
2017-09-05 00:38:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {};
+var playlist = { Weezer: "The Good Life" };
 
 function updatePlaylist(playlist,artistName,songTitle) {
   return Object.assign({}, playlist, { [artistName]: songTitle })
